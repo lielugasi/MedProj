6cc99b115fbf44b900b994cb94ea06ea9c49dce6
ליאל לוגסי <lielugasi@gmail.com>
2020-10-08 20:31:27 +0300

    first commit

---
+++ FinalProject/App_Start/RouteConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FinalProject
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "admind",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Admin", action = "Admin", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "doc",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Doctor", action = "Doctor", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+              name: "client",
+              url: "{controller}/{action}/{id}",
+              defaults: new { controller = "Client", action = "Client", id = UrlParameter.Optional }
+          );
+        }
+    }
+}
