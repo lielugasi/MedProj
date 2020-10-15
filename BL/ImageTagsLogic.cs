d6d7d2ba371bc379ef01ba326977b484bc9ed01a
ליאל לוגסי <lielugasi@gmail.com>
2020-10-15 15:11:57 +0300

    שינוי

---
+++ BL/ImageTagsLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BL
+{
+    public class ImageTagsLogic
+    {
+        public void Uplaod(HttpPostedFileBase file)
+        {
+            GoogleDriveAPIHelper g = new GoogleDriveAPIHelper();
+            g.UplaodFileOnDrive(file);
+
+        }
+    }
+}
---
+++ FinalProject/Controllers/MineController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class MineController : Controller
+    {
+        // GET: My
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            BL.ImageTagsLogic j = new BL.ImageTagsLogic();
+            j.Uplaod(file);
+            return View();
+        }
+    }
+}
